8177fb35a50b04b7fb5066a2a9dc67550ab1d335
senanito85 <sysad85@gmail.com>
2020-05-17 21:21:43 +1000

    added calculations

--- vulevue/hellow.go
+++ vulevue/hellow.go
@@ -4,4 +4,16 @@ import "fmt"
 
 func main() {
 	fmt.Println("hello")
+	a := 20.45
+	b := 34.89
+
+	// Subtraction of two
+	// floating-point number
+	c := b - a
+
+	// Display the result
+	fmt.Printf("Result is: %f", c)
+
+	// Display the type of c variable
+	fmt.Printf("\nThe type of c is : %T", c)
 }
